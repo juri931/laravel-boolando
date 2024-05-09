36edeb188537c7573bdb622f55eb13ddd8fd8534
juri931 <impa.mat@hotmail.it>
2024-05-09 16:40:36 +0200

    "Move cards display from separate 'cards' view to home view; update blade syntax to use array syntax for array variables"
    Explanation:
    The code diff shows the moving of the display of cards from a separate 'cards' view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,24 @@
 @extends('layouts.main')
 
+@section('content')
+    <div class="container">
+        @foreach ($cards as $card)
+            <div class="product">
+                <img class="primary-img" src="{{ $card['img'] }}" />
+                <img class="img-hover" src="{{ $card['imgHover'] }}" />
+                <div class="disc-eco">
+                    <span class="discount">{{ $card['discount'] }}</span>
+                    <span class="eco">{{ $card['eco'] }}</span>
+                </div>
+                <span class="heart">&hearts;</span>
 
+                <div class="info">
+                    <span class="brand">{{ $card['brand'] }}</span>
+                    <span class="desc">{{ $card['desc'] }}</span>
+                    <span class="price-new">{{ $card['priceNew'] }}</span>
+                    <span class="price-old">{{ $card['priceOld'] }}</span>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,9 +5,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css' integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==' crossorigin='anonymous'/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/  font-awesome/6.5.1/css/all.min.css"
+    integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl  +Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('resources/js/app.js')
 
-    <title>Laravel Boolando | @yield('title')</title>
+    <title>Laravel Boolando</title>
 
 </head>
 
